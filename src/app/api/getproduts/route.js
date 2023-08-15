81c53583ed34ce3d33a5f3f14bb65c084c62e3ce
Mimran-ship-it <126707772+Mimran-ship-it@users.noreply.github.com>
2023-08-15 17:51:35 +0500

    msms

--- src/app/api/getproduts/route.js
+++ src/app/api/getproduts/route.js
@@ -1,44 +1,46 @@
-import connecttodatabase from '../../../db/connection'
-import Product from '../../../Collections/Product'
+import connectToDatabase from '../../../db/connection';
+import Product from '../../../Collections/Product';
 import { NextResponse } from 'next/server';
 
-export async function GET(){
- await connecttodatabase()
-  
-  const data = await Product.find();
-    let datareq={}
-    data.map((ele)=>{
-      let name=ele.name;
-      let size=ele.size;  
-      let color=ele.color;  
-      if(name in datareq){
-datareq[name]={
-         name: ele.name,
-        category: ele.category,
-        quantity: ele.quantity,
-        price:ele.price,
-        slug:ele.slug,
-        size:{...datareq[name].size, [size]: [...(datareq[name].size[size] || []), color],},
-        desc: ele.desc,
-        image:{...datareq[name].image,[color]:ele.image}
-}
-      }else{
-      datareq[name]={
-        name: ele.name,
-        category: ele.category,
-        quantity: ele.quantity,
-        slug:ele.slug,
-        size:{[size]:[color]},
-        price:ele.price,
-        desc: ele.desc,
-        image:{[color]:ele.image}
+export async function GET() {
+  try {
+    await connectToDatabase(); // Connect to the database
 
+    const data = await Product.find();
+    let datareq = {};
 
+    data.forEach((ele) => {
+      let name = ele.name;
+      let size = ele.size;
+      let color = ele.color;
+      
+      if (!datareq[name]) {
+        datareq[name] = {
+          name: ele.name,
+          category: ele.category,
+          quantity: ele.quantity,
+          price: ele.price,
+          slug: ele.slug,
+          size: {},
+          desc: ele.desc,
+          image: {},
+        };
       }
+      
+      if (!datareq[name].size[size]) {
+        datareq[name].size[size] = [color];
+      } else {
+        datareq[name].size[size].push(color);
       }
-    })
+
+      if (!datareq[name].image[color]) {
+        datareq[name].image[color] = ele.image;
+      }
+    });
 
     return NextResponse.json({ datareq });
-  
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return NextResponse.json({ error: 'An error occurred while fetching data.' }, 500);
+  }
 }
-
