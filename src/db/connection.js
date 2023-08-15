769cbb6250ca9a0e9bbb6613ea200e2805f913e9
Mimran-ship-it <126707772+Mimran-ship-it@users.noreply.github.com>
2023-08-15 17:40:24 +0500

    sum

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,13 +1,21 @@
 import mongoose from "mongoose";
-const connectToDatabase =async (req,res)=>{
-  if(mongoose.connections[0].readyState){
-    console.log('connected')
-   
+
+const connectToDatabase = async () => {
+  try {
+    if (mongoose.connections[0].readyState) {
+      console.log('Already connected to the database.');
+      return;
+    }
+    
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+
+    console.log('Connected to the database.');
+  } catch (error) {
+    console.error('Database connection error:', error);
   }
-  await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true , autoIndex: true})
- 
-}
-
-
+};
 
 export default connectToDatabase;
